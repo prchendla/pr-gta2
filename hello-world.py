eb4ce4c48732e262c20bd15b8f8abae6027d360f
prchendla <pratham.devops@gmail.com>
2022-10-18 19:34:33 +0530

    KT1

--- hello-world.py
+++ hello-world.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Good night</h1>"
+    return "<h1 style='color:blue'>Good night1</h1>"
     print("hello")
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3007)
